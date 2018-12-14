b4bc58b5309aa2cc9291aa2e0b9bbbf629022eed
Harshit <chauhan439@gmail.com>
2018-12-14 07:05:01 +0530

    Heroku Config 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ app= express() ;
 const PORT= process.env.PORT || 3000 ;
 
 app.use (express.static ("public")) ;
-app.use (function (req, res, next)){
+app.use (function (req, res, next){
     if (req.headers ['x-forwarded-proto'] === 'http'){
 
       next () ;
@@ -11,7 +11,8 @@ app.use (function (req, res, next)){
     else{
       res.redirect ("http://" + req.hostname + req.url) ;
     }
-};
+});
+
 app.listen (PORT, function () {
   console.log ("Listening at "+ PORT) ;
 }) ;
