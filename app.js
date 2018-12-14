1fd5ec4c8d05c6e81a4e8231cf80604d5efd2ecd
Harshit <chauhan439@gmail.com>
2018-12-14 09:43:37 +0530

    Fix Weather API

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var express= require ("express") ;
-app= express() ;
-const PORT= process.env.PORT || 3000 ;
+var express = require('express');
 
-app.use (express.static ("public")) ;
-app.use (function (req, res, next){
-    if (req.headers ['x-forwarded-proto'] === 'http'){
+var app = express();
+const PORT = process.env.PORT || 3000;
 
-      next () ;
-    }
-    else{
-      res.redirect ("http://" + req.hostname + req.url) ;
-    }
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
 });
 
-app.listen (PORT, function () {
-  console.log ("Listening at "+ PORT) ;
-}) ;
+app.use(express.static('public'));
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -2,7 +2,7 @@ var axios = require('axios');
 
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
 
-module.exports = {  
+module.exports = {
   getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
